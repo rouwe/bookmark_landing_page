68b8cc8eac816aaf04bbf6b33f9e2fbc05807049
rouwe <santosroweme14@gmail.com>
2022-01-02 13:31:34 +0800

    Contact section email validation and interactivity update

---
+++ js/email_validation.js
@@ -0,0 +1,24 @@
+// Contact Section
+const contactEmail = document.getElementById('email');
+const contactSubmit = document.getElementsByClassName('contact-btn')[0];
+const emailFieldContainer = document.getElementsByClassName('email-field-container')[0];
+const emailFieldErrorSvg = emailFieldContainer.getElementsByTagName('svg')[0];
+const contactError = document.getElementsByClassName('email-error')[0];
+// Add event listener
+contactSubmit.addEventListener('click', validateEmail);
+let email = document.contactForm.userEmail;
+
+function validateEmail() {
+    // Validate email
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    email = email.value;
+    if (email.match(regex)) {
+        contactError.style.display = 'none';
+        emailFieldErrorSvg.style.display = 'none';
+        return true;
+    } else {
+        contactError.style.display = 'block';
+        emailFieldErrorSvg.style.display = 'block';
+        return false;
+    }
+}
